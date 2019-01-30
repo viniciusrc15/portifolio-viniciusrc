027936712d7843a88f5d0b04406e2304b7c11b7f
Vinicius Castro <vinicius1595@gmail.com>
2019-01-30 14:21:21 -0200

    better layout and experiences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { ProfileComponent } from './profile/profile.component';
+import { HistoryComponent } from './history/history.component';
+import { SkillsComponent } from './skills/skills.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HistoryComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule, SharedModule, AppRoutingModule

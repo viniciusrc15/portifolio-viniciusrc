69f900878e3b08f5424feb557f5f38f5af92317a
Vinicius Castro <vinicius1595@gmail.com>
2019-02-01 15:59:56 -0200

    init skills

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  year;
+  constructor() {
+    this.year = new Date();
+  }
 
   ngOnInit() {
+    this.year = this.year.getFullYear() - 1996;
   }
 
 }
